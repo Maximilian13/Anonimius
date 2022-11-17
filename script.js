f4b864ce183f2357c68d18e69b160c90a788bc20
Maximilian13 <maxim.myagchilo@gmail.com>
2022-11-17 16:27:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let input = document.querySelector("input")
 
 btn.onclick = () => {
 	if (input.value == "вектор") {
-		window.location.replace("finish.html");
+		window.location.replace("finish1307.html");
 	} else {
 		input.value = "Неправильно!"
 	}
